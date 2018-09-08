7fbca11a22035c9a98a162c130bdca93c65c5902
Shiju John <shijujohn1093@gmail.com>
2018-09-09 03:02:21 +0530

    first commit

---
+++ src/main/java/com/thengara/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.thengara.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}
