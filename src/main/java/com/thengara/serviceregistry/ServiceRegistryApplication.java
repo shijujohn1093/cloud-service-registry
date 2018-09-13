56340835e9c9f2ef0e74c0cbd81f76b53f169af5
Shiju John <shijujohn1093@gmail.com>
2018-09-13 06:09:50 +0530

    chnage in registry and config moved to repository

--- src/main/java/com/thengara/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/thengara/serviceregistry/ServiceRegistryApplication.java
@@ -2,9 +2,7 @@ package com.thengara.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableEurekaServer
